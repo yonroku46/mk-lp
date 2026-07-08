55e3ec37b345a7fc246816e736f8961da9ba1ab2
4ROKU <71884535+yonroku46@users.noreply.github.com>
2026-07-08 11:13:39 +0900

    price page add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/price',
+        destination: '/docs/price.pdf',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
